9a3af6b26d1aa259462e2493c32091dc5f3ba23c
Rodomond Olivier <rodomond.o@gmail.com>
2016-04-23 15:48:27 +0700

    init google translate lib

--- app.js
+++ app.js
@@ -2,17 +2,39 @@
 
 var 
   app = require('express')(),
-  bodyParser = require('body-parser')
+  config = require('config'),
+  bodyParser = require('body-parser'),
+  google = require('googleapis'),
+  translate = google.translate('v2');
   app.use( bodyParser.json() );
   app.use(bodyParser.urlencoded({extended: true})); 
+
+
 
 app.get('/', function (req, res) {
   res.send('hello world');
 });
 
+app.post('/:lang', function (req, res) {
+  var options = {
+    key : config.google.key,
+    q : req.body.text,
+    target : req.params.lang
+  };
 
-app.listen(process.env.OPENSHIFT_NODEJS_PORT || 8080, process.env.OPENSHIFT_NODEJS_IP, function () {
-    console.log('Example app listening on port 3000!');
+  if (!options.key) return res.send('need google key');
+  if (!options.q) return res.send('need query');
+  if (!options.target) return res.send('need target');
+
+  translate.translations.list(options, function(err, response) {
+    var result = response.data.translations;
+    return res.send(result);
+  });
+});
+
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+app.listen(port, process.env.OPENSHIFT_NODEJS_IP, function () {
+    console.log('Example app listening on port '+ port+ '!');
 });
 
 
