24afa7183c54dea06e2391af56a41618dd0fccc1
Rodomond Olivier <rodomond.o@gmail.com>
2016-04-23 23:33:27 +0700

    update config for heroku

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.post('/:lang', function (req, res) {
   });
 });
 
-var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var port = process.env.PORT || 5000;
 app.listen(port, process.env.OPENSHIFT_NODEJS_IP, function () {
     console.log('Example app listening on port '+ port+ '!');
     bot({
